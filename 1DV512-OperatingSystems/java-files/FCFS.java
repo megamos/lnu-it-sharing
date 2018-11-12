76641363742aa949420251f60ccc3c378de892a5
megamos <mm.swedev@gmail.com>
2018-11-12 09:34:19 +0100

    Starting OS course

---
+++ 1DV512-OperatingSystems/java-files/FCFS.java
@@ -0,0 +1,35 @@
+/*
+ * File:	Process.java
+ * Course: 	Operating Systems
+ * Code: 	1DV512
+ * Author: 	Suejb Memeti
+ * Date: 	November, 2018
+ */
+
+import java.util.ArrayList;
+
+public class FCFS{
+
+	// The list of processes to be scheduled
+	public ArrayList<Process> processes;
+
+	// Class constructor
+	public FCFS(ArrayList<Process> processes) {
+		this.processes = processes;
+	}
+
+	public void run() {
+		// TODO Implement the FCFS algorithm here
+
+	}
+
+	public void printTable() {
+		// TODO Print the list of processes in form of a table here
+
+	}
+
+	public void printGanttChart() {
+		// TODO Print the demonstration of the scheduling algorithm using Gantt Chart
+
+	}
+}
